0ba8b71bd9cdae4e6e0827b6953a06287bbf5d96
dylangouzenne <dylan.gouzenne@gmail.com>
2021-07-07 12:03:31 +0200

    ajout dockerfile app NodeJS + création fichier app.js

---
+++ app_webhook/app.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var xhub = require("express-x-hub");
+
+//Const
+var xhubSecret = "MySecretKey";
+var port = "8085";
+var host = "localhost";
+
+//Secret key
+app.use(xhub({ algorithm: "sha1", secret: xhubSecret }));
+
+// Configure express json
+app.use(bodyParser.json());
+
+// Main : Start the express http server
+var server = app.listen(port, host, function() {
+  console.log(
+    "App listening at http://%s:%s",
+    server.address().address,
+    server.address().port,
+  );
+});
+
+// Add default route
+app.post("/webhook", function(req, res) {
+  if (!req.isXHubValid()) {
+    res.status(400).send("Invalid X-Hub Request");
+    console.log("Secret key is invalid");
+    return;
+  }
+
+  var command = req.headers["x-github-event"];
+
+  switch (command) {
+    //Event create (Branch, or tag created)
+    case "create":
+      res.send("Event create trigger");
+      console.log("Create event");
+      break;
+
+    //Event release (Release published in a repository)
+    case "release":
+      res.send("Event release trigger");
+      console.log("Release Event");
+      break;
+
+    default:
+      res.status(400).send("Event not supported : " + command);
+      console.log("Event not supported : " + req.headers["X-Github-Event"]);
+  }
+});
